a4ce85d1e725ac409451da049211708ee0770165
denis.pchelintsev <denis.pchelintsev@softline.com>
2025-01-16 16:29:27 +0500

    Добавил: - конфигурацию на подключение разных логгеров и адаптеров к разным уровням логирования - реализовал 2 аспекта логирования:      а. простой - логирует только вход и выход из вэб-метода, и время выполнения      б. детальный - логирует так же входящие аргументы, результат метода и ошибки

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,9 @@ dependencies {
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	// для АОП (подключение Аспектов)
+	implementation("org.springframework.boot:spring-boot-starter-aop")
 }
 
 dependencyManagement {
---
+++ src/main/java/com/gravitee/example/aspect/EntryExitLoggingAspect.java
@@ -0,0 +1,49 @@
+package com.gravitee.example.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * Аспект логирования фактов входа в метод и выхода из метода (web-метод) REST-контроллера.
+ * Если логи настоящего аспекта связывать напрямую по TraceID, а не по временным отметкам -
+ *  то такие логи представляют путь, которым проходит сигнал (сообщение, Заявка) через Сервисы.
+  */
+@Aspect
+@Component
+public class EntryExitLoggingAspect {
+
+    /** Логгер аспекта.
+     *      Так.как аспект в run-time явзяется демоном - то, на этот логгер не влияют исключительные ситуации в
+     *      приложении (основной процесс).
+     */
+    private static final Logger logger = LoggerFactory.getLogger(EntryExitLoggingAspect.class);
+
+    /**
+     * Непосредственно Асект, "обрамляющий" (контроль до вызова метода, и выход из метода) каждый метод контроллера.
+     * В логах остаются отметки:
+     *  - факт входа в метод
+     *  - факт выхода из метода
+     *  - время, потраченное на выполнение метода
+     * @param joinPoint
+     * @return
+     * @throws Throwable
+     */
+    @Around("execution(* com.gravitee.example.controller.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();                    // фиксируем время начала
+        // фиксируем вход в метод
+        logger.info("Entering method {}", joinPoint.getSignature().getName());
+        // выполнение целевого метода
+        Object proceed = joinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - start;    // фиксируем время выполнения
+        // фиксируем время выполнения целевого метода
+        logger.info("Method {} executed in {} ms", joinPoint.getSignature().getName(), executionTime);
+        // фиксируем выход из метода
+        logger.info("Exiting method {}", joinPoint.getSignature().getName());
+        return proceed;
+    }
+}
